32ca2aab258dd11a4b70ed9587ab8561f3a6ea72
tiagohelel <tiago_helel@hotmail.com>
2019-12-25 10:26:38 -0300

    Fisrt

---
+++ core/api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework.serializers import ModelSerializer
+from core.models import PontoTuristico
+from atracoes.api.serializers import AtracaoSerializer
+from enderecos.api.serializers import EnderecoSerializer
+from rest_framework.fields import SerializerMethodField
+
+class PontoTuristicoSerializer(ModelSerializer):
+    atracoes = AtracaoSerializer(many=True)
+    endereco = EnderecoSerializer()
+    descricao_completa = SerializerMethodField()
+
+    class Meta:
+        model = PontoTuristico
+        fields = [
+            'id', 'nome', 'descricao', 'aprovado', 'endereco', 'foto',
+            'atracoes', 'comentarios', 'avaliacoes', 'endereco', 
+            'descricao_completa', 'descricao_completa2'
+            ]
+
+    def get_descricao_completa(self, obj):
+        return f'{obj.nome} - {obj.descricao}'
