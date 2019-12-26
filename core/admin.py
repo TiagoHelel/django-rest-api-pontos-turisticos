ad177c7858ba826114378cf5d7af7cc8d8f99b25
tiagohelel <tiago_helel@hotmail.com>
2019-12-26 00:21:50 -0300

    Novas funcionalidades

--- core/admin.py
+++ core/admin.py
@@ -3,5 +3,4 @@ from django.contrib import admin
 from .models import PontoTuristico
 from .models import DocIdentificacao
 
-admin.site.register(PontoTuristico)
-admin.site.register(DocIdentificacao)
+admin.site.register(PontoTuristico, DocIdentificacao)
